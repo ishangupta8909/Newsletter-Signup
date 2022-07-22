e8903d89cf6af41a592e311a067423afb926c5aa
Ishan Gupta <ishangupta8909@gmail.com>
2022-07-22 10:51:07 +0530

    This is my first commit

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.post("/failure.html", function(req,res){
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running at 3000");
 });
 
